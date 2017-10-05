2e0846dd900d7ae566ca0a13a06be6cff7df48eb
Pablo Sánchez <pabloalex@gmail.com>
2017-10-05 12:41:37 +0200

    Initial Commit

---
+++ Pid_Agent.m
@@ -0,0 +1,40 @@
+classdef Pid_Agent < handle
+    
+    properties
+        genome = rand(3,1).*5;
+        score = 0;
+        tf;
+        sys;
+        info;
+    end
+ 
+    methods
+        
+        function agent = Pid_Agent(tf)
+            agent.tf = tf;
+        end
+        
+        function new_agent = mate(this,other)
+
+            new_agent = Pid_Agent(this.tf);
+            new_agent.genome = this.genome + other.genome;
+            new_agent.genome = new_agent.genome ./ 2;
+            for i=1:3
+                if rand() < 0.01
+                    exp = (-1) ^ round(rand() *5);
+                    new_agent.genome(i) = new_agent.genome(i) + exp * rand() * new_agent.genome(i);
+                end
+            end
+        end
+        
+        function score = getScore(this)
+            pid_ = pid(this.genome(1),this.genome(2),this.genome(3));
+            this.sys = feedback(pid_*this.tf,1);
+%             step(this.sys);
+%             pause;
+            this.info = stepinfo(this.sys);
+            this.score = 1 / (1 + this.info.Overshoot + this.info.RiseTime + this.info.Undershoot +  1 ) ^ 2;
+            score = this.score;
+        end
+    end
+end
---
+++ gen_pid.m
@@ -0,0 +1,50 @@
+%% Init
+clear
+clc
+close all
+%% params
+population_size = 100;
+population_to_keep = 30;
+
+%% System
+TF = tf([2 1],[1 2 1]);
+
+%% initial population
+
+for i=1:population_size
+    population(i) = Pid_Agent(TF);
+end
+
+%% loop
+
+f = figure;
+while true
+    % simulate population & create mating pool
+    best_score = 0;
+    k = 1;
+    for i=1:population_size
+        score = population(i).getScore();
+        if score > best_score
+            best = population(i);
+        end
+        for j=1:round(score*100)
+            mating_pool(k) = population(i);
+            k = k +1;
+        end
+    end
+    % show best response
+    fprintf('Kp: %f Ki: %f Kd: %f. O: %f RT: %f U: %f\n',best.genome(1),best.genome(2),best.genome(3), ...
+        best.info.Overshoot,best.info.RiseTime, best.info.Undershoot);
+    step(best.sys);
+    pause(0.01);
+    % keep some agents from old population
+    for i=1:population_to_keep
+        population(i) = mating_pool(floor(rand()*(k-1)+1));
+    end
+    % mate
+    for p=population_to_keep+1:population_size
+        index_a = floor(rand()*(k-1)+1);
+        index_b = floor(rand()*(k-1)+1);
+        population(p) = mating_pool(index_a).mate(mating_pool(index_b));
+    end
+end
